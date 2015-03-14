56b6972734e9986cac70fd2ef091075cfbe02400
Kevin Isom <kevin.isom@gmail.com>
2015-03-14 16:47:28 +1300

    actual root bar graph component

---
+++ components/bar-graph.jsx
@@ -0,0 +1,18 @@
+class BarGraph extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props);
+        console.log(this.state);
+        var calculateRange = function (data) {
+          
+        }
+        return (
+            <svg className="graph">
+              <Grid></Grid>
+            </svg>
+        );
+    }
+}
